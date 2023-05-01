9b470a056ae0c14c449c920e9182614648301703
Daniel Carling <danielarlindocarling@hotmail.com>
2023-05-01 14:29:59 -0400

    remove all

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-   reactStrictMode: true,
-   pageExtensions: ['page.tsx', 'page.ts', 'api.tsx', 'api.ts'],
- }
- 
- module.exports = nextConfig
