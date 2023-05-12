e3abc91b24eaecccb356fdfe0dcf3cdb4201506b
Daniel Carling <117768647+danielcarling@users.noreply.github.com>
2023-05-12 14:48:18 -0400

    update exercize

--- src/components/global/PaymentForm/index.tsx
+++ src/components/global/PaymentForm/index.tsx
@@ -109,7 +109,6 @@ export default function PaymentForm() {
                      id="price"
                      value={price}
                      onChange={handlePriceChange}
-                     min={0}
                      required
                   />
                </div>
@@ -119,17 +118,17 @@ export default function PaymentForm() {
                   {/* <input type="number" name="parcels" id="parcels" min={0} required /> */}
                   <select name="parcels" id="parcels">
                      <option value="1">1</option>
-                     <option value="1">2</option>
-                     <option value="1">3</option>
-                     <option value="1">4</option>
-                     <option value="1">5</option>
-                     <option value="1">6</option>
-                     <option value="1">7</option>
-                     <option value="1">8</option>
-                     <option value="1">9</option>
-                     <option value="1">10</option>
-                     <option value="1">11</option>
-                     <option value="1">12</option>
+                     <option value="2">2</option>
+                     <option value="3">3</option>
+                     <option value="4">4</option>
+                     <option value="5">5</option>
+                     <option value="6">6</option>
+                     <option value="7">7</option>
+                     <option value="8">8</option>
+                     <option value="9">9</option>
+                     <option value="10">10</option>
+                     <option value="11">11</option>
+                     <option value="12">12</option>
                   </select>
                </div>
             </FormContentContainer>
